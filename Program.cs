6b21be9e278bb6ccfa05eeb03be03d0b7f449c1f
rmk062026 <rmk062026@gmail.com>
2026-09-03 20:09:50 +0200

    Testing with stringRepository

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
 ﻿namespace GenericsAssignment;
 
+using GenericsAssignment.Repositorys;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Repository<string> stringRepository = new();
+
+        stringRepository.Add("HEI");
+        stringRepository.Add("Verden!");
+
+        stringRepository.Remove("Verden!");
+        foreach (string item in stringRepository.GetAll())
+        {
+            Console.WriteLine(item);
+        }
     }
 }
--- Repositories/Repository.cs
+++ Repositories/Repository.cs
@@ -1,6 +1,22 @@
-namespace GenericsAssignment.Repository;
+using GenericsAssignment.interfaces;
 
-public class Repository<T>
+namespace GenericsAssignment.Repositorys;
+
+public class Repository<T> : IRepository<T>
 {
     private List<T> items = new();
+    public void Add(T item)
+    {
+        items.Add(item);
+    }
+
+    public List<T> GetAll()
+    {
+        return items;
+    }
+
+    public bool Remove(T item)
+    {
+        return items.Remove(item);
+    }
 }
